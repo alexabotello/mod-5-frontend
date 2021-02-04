8aaf47fb088bcb2a666c0c0133b1682bcbc9e8b8
alexabotello <abotello20@yahoo.com>
2021-02-04 11:37:03 -0600

    first commit

---
+++ src/Homepage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css';
+
+class Homepage extends React.Component {
+  render(){
+      return(
+    <React.Fragment>
+       <div className='homepage'>
+           <h1>hello</h1>
+       </div>
+    </React.Fragment>
+    )
+  }
+}
+
+export default Homepage;
